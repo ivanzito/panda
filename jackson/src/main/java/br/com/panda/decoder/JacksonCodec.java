f14931627c25e3be715e3d45eb566726a5b61614
Ivan Rodrigues <ivanzito.jr@gmail.com>
2022-11-01 07:59:21 -0300

    test: pmd

--- jackson/src/main/java/br/com/panda/decoder/JacksonCodec.java
+++ jackson/src/main/java/br/com/panda/decoder/JacksonCodec.java
@@ -8,10 +8,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JacksonCodec implements Decoder, Encoder {
 
+    private String test = null;
+
     private final ObjectMapper mapper = new ObjectMapper();
 
     @Override
     public <T> T decode(final String value, final Class<T> clazz) {
+        System.out.println(test.toCharArray()[10]);
         try {
             return mapper.readValue(value, clazz);
         } catch (JsonProcessingException e) {
